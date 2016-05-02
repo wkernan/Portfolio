bd98067c7f0c4af4cc33c7d86d1fd93543e5eb72
wkernan@gmail.com <wkernan@gmail.com>
2016-05-02 14:38:51 -0500

    redo email test

--- assignment2/HTML/contact.php
+++ assignment2/HTML/contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = wkernan@gmail.com;
-$siteTitle = Bill Kernan;
+$emailTo = 'wkernan@gmail.com';
+$siteTitle = 'Bill Kernan';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
